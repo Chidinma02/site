035567e153e4c87af60dba1d9e55d609bff31ecc
Chidinma <nancee801@gmail.com>
2021-06-07 14:07:26 +0100

    commit

---
+++ js/javascript.js
@@ -0,0 +1,36 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";4
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  .ra{
+    margin-top:-60px;
+  }
+  <form>
+                    <div class="form-group">
+                      <label for="exampleFormControlInput1">full Name</label>
+                      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="full Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Email </label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email ">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Mobile Number</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Mobile Number">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Password</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Password"> </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Repeat Password</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Repeat Password">
+                        </div>
+
+                        <button type="button" class="btn btn-primary ">Register</button>
+                        <button type="button" class="btn btn-primary ju ">Login</button>
+                  </form>
